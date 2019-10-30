58f5192eb0cf8c7553e007d16bb5605709664fb1
zegenerative <zegerdevos@gmail.com>
2019-10-30 17:28:57 +0100

    Add description model

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import * as ml5 from "ml5";
 
-const modelPath = 'http://localhost:3000/models/dinomodel'
+const dinoNamesModel = 'http://localhost:3000/models/DinoModel'
+const dinoDescriptionsModel = 'http://localhost:3000/models/DinoDescriptionModel'
 
 class App extends Component {
   state = {
-    rnn: {},
+    nameModel: {},
+    descriptionModel: {},
     input: '',
-    result: ''
+    resultName: '',
+    resultDescription: ''
   }
 
-  charRNN = async () => {
-    // const rnn = await ml5.charRNN(model, modelLoaded)
-    // also does not work:
-    const rnn = await ml5.charRNN(modelPath, modelLoaded)
+  nameModel = async () => {
+    const nameModel = await ml5.charRNN(dinoNamesModel, nameModelLoaded)
     this.setState({
-      rnn,
+      nameModel,
     }) 
 
-    function modelLoaded() {
-      console.log('Model Loaded!');
+    function nameModelLoaded() {
+      console.log('Name model loaded!');
+    }
+  }
+
+  descriptionModel = async () => {
+    const descriptionModel = await ml5.charRNN(dinoDescriptionsModel, descriptionModelLoaded)
+    this.setState({
+      descriptionModel,
+    }) 
+
+    function descriptionModelLoaded() {
+      console.log('Description model loaded!');
     }
   }
 
   componentDidMount(){
-    this.charRNN();
+    this.nameModel()
+    this.descriptionModel()
   }
 
   onChange = (event) => {
@@ -40,32 +53,53 @@ class App extends Component {
       temperature: 0.5,
       length: 20
     }
-    const results = await this.state.rnn.generate(data, function(err, results) {
-        console.log(results)
+    const results = await this.state.nameModel.generate(data, function(err, results) {
         return results
     })
     this.setState({
-      result: this.state.input + results.sample
+      resultName: (this.state.input + results.sample).toUpperCase()
+    })
+  }
+
+  onClick = async () => {
+    const data = { 
+      seed: this.state.input,
+      temperature: 0.01,
+      length: 300
+    }
+    const results = await this.state.descriptionModel.generate(data, function(err, results) {
+        return results
+    })
+    this.setState({
+      resultDescription: results.sample
     })
   }
 
   render() {
     return (
       <div>
-        <h1>Create a Dino name</h1>
-        <form onSubmit={this.onSubmit}> 
-          <label>Input:
-              <input 
-                  type="text" 
-                  onChange={this.onChange}
-                  value={this.state.input}
-                  name='input'
-                  placeholder='input'
-              />
-          </label>
-          <button>Generate</button>
-          <p>{ this.state.result !== '' ? this.state.result : '' }</p>
-        </form>
+        <div>
+          <h1>THE DINOSAURIFIER</h1>
+          <h4>Create your dinosaur name:</h4>
+          <form onSubmit={this.onSubmit}> 
+            <label>Name:
+                <input 
+                    type="text" 
+                    onChange={this.onChange}
+                    value={this.state.input}
+                    name='input'
+                    placeholder='your first name'
+                />
+            </label>
+            <button>Generate</button>
+            <p>{ this.state.resultName !== '' ? this.state.resultName : '' }</p>
+          </form>
+        </div>
+        <div>
+          <h4>Create a description:</h4>
+          <p>{this.state.resultDescription}</p>
+          <button onClick={this.onClick}>Generate</button>
+        </div>
       </div>
     );
   }
